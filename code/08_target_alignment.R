91bcb2736048c1c10bf0413117c0427d033c947d
Mert Cihan <mcihan.bioinf@gmail.com>
2025-08-26 15:40:26 +0200

    Update scripts and files

--- code/08_target_alignment.R
+++ code/08_target_alignment.R
@@ -1,98 +1,152 @@
 # Load required libraries
-library(Biostrings)
-library(GenomicRanges)
-library(stringr)
-library(dplyr)
-library(readr)
 library(biomaRt)
+library(Biostrings)
+library(data.table)
+library(GenomicRanges)
+library(GenomicFeatures)
+library(ensembldb)
+library(EnsDb.Hsapiens.v86)
 
-# Load validated miRNA-gene interactions
-interactions <- read_csv("validated_interactions.csv")
+# Load validated interactions
+interactions <- fread("validated_interactions.csv")
+setnames(interactions, c("miRNA","Target_Gene"))
 
-# Load and convert miRNA seed sequences from RNA to DNA
-mirna_seeds <- readRNAStringSet("mirna_seed.fasta")
-mirna_seeds_dna <- as(DNAStringSet(RNAStringSet(mirna_seeds)), "DNAStringSet")
-mirna_seeds_dna <- mirna_seeds_dna[names(mirna_seeds_dna) %in% interactions$miRNA]
-
-# Function to fetch 3' UTR genomic locations using biomaRt
-get_utr_genomic_locations <- function(gene_list, batch_size = 50) {
-  mart <- useMart("ensembl", dataset = "hsapiens_gene_ensembl")
-  utr_data_list <- list()
-  
-  for (i in seq(1, length(gene_list), by = batch_size)) {
-    batch_genes <- gene_list[i:min(i + batch_size - 1, length(gene_list))]
-    tryCatch({
-      utr_batch <- getBM(
-        attributes = c("hgnc_symbol", "chromosome_name", "transcript_start", "transcript_end", "strand", "3utr"),
-        filters = "hgnc_symbol",
-        values = batch_genes,
-        mart = mart
-      )
-      utr_data_list[[length(utr_data_list) + 1]] <- utr_batch
-    }, error = function(e) message("Error: ", e$message))
-  }
-  
-  do.call(rbind, utr_data_list)
+# Fetch canonical 3'UTRs
+mart <- useMart("ENSEMBL_MART_ENSEMBL",
+                dataset = "hsapiens_gene_ensembl",
+                host = "https://www.ensembl.org")
+attrs <- c("hgnc_symbol","ensembl_transcript_id","chromosome_name","strand",
+           "transcript_start","transcript_end","3utr","transcript_is_canonical")
+target_genes <- sort(unique(interactions$Target_Gene))
+batch_size <- 300
+chunks <- split(target_genes, ceiling(seq_along(target_genes)/batch_size))
+utr_list <- list()
+for (i in seq_along(chunks)) {
+  utr_list[[i]] <- getBM(attributes = attrs,
+                         filters = "hgnc_symbol",
+                         values = chunks[[i]], mart = mart)
 }
+utr_data <- as.data.table(rbindlist(utr_list, use.names=TRUE, fill=TRUE))
+utr_data <- utr_data[transcript_is_canonical == 1]
+setnames(utr_data, names(utr_data), sub("^X3utr$","3utr",names(utr_data)))
+utr_data <- utr_data[!is.na(`3utr`) & `3utr` != "" & `3utr` != "Sequence unavailable"]
+utr_data[, utr_len := nchar(`3utr`)]
+utr_data <- utr_data[chromosome_name %in% c(as.character(1:22),"X","Y","MT","M")]
+setorder(utr_data,hgnc_symbol,-utr_len)
+utr_data <- utr_data[!duplicated(hgnc_symbol)]
+utr_data[, strand := as.integer(strand)]
+utr_data[, `:=`(transcript_start=as.integer(transcript_start),
+                transcript_end=as.integer(transcript_end))]
 
-# Get and process UTR sequences
-target_genes <- unique(interactions$`Target Gene`)
-utr_data <- get_utr_genomic_locations(target_genes) %>%
-  filter(`3utr` != "Sequence unavailable") %>%
-  mutate(utr_length = nchar(`3utr`)) %>%
-  arrange(hgnc_symbol, desc(utr_length)) %>%
-  distinct(hgnc_symbol, .keep_all = TRUE)
+# Build UTR DNAStringSet
+utr_set <- DNAStringSet(utr_data$`3utr`)
+names(utr_set) <- utr_data$hgnc_symbol
 
-# Function to find genomic binding sites for a miRNA-gene pair
-find_genomic_matches <- function(mirna_name, seed_seq, target_gene, utr_data) {
-  gene_data <- utr_data[utr_data$hgnc_symbol == target_gene, ]
-  if (nrow(gene_data) == 0) return(NULL)
-  
-  chrom <- gene_data$chromosome_name
-  strand <- gene_data$strand
-  utr_seq <- gene_data$`3utr`
-  utr_len <- nchar(utr_seq)
-  
-  utr_genomic_start <- if (strand == 1) gene_data$transcript_end - utr_len + 1 else gene_data$transcript_start
-  utr_genomic_end <- if (strand == 1) gene_data$transcript_end else gene_data$transcript_start + utr_len - 1
-  
-  matches <- matchPattern(seed_seq, DNAString(utr_seq))
-  if (length(matches) == 0) return(NULL)
-  
-  match_df <- as.data.frame(ranges(matches))
-  match_df$genomic_start <- if (strand == 1) utr_genomic_start + match_df$start - 1 else utr_genomic_end - match_df$end + 1
-  match_df$genomic_end <- if (strand == 1) utr_genomic_start + match_df$end - 1 else utr_genomic_end - match_df$start + 1
-  match_df$miRNA <- mirna_name
-  match_df$Target_Gene <- target_gene
-  match_df$Chromosome <- chrom
-  match_df$Strand <- strand
-  
-  match_df
+# Load miRNAs
+mirna_raw <- readRNAStringSet("/mature_mirnas.fa", format="fasta")
+mirna_dna <- DNAStringSet(RNAStringSet(mirna_raw))
+names(mirna_dna) <- sub(" .*","",trimws(names(mirna_dna)))
+mirna_dna <- mirna_dna[grepl("^hsa-",names(mirna_dna))]
+mirna_dna <- mirna_dna[names(mirna_dna) %in% interactions$miRNA]
+
+# Build seed dictionaries
+seed6   <- DNAStringSet(lapply(mirna_dna,function(s) subseq(s,2,7)))
+seed7m8 <- DNAStringSet(lapply(mirna_dna,function(s) subseq(s,2,8)))
+seed7A1 <- DNAStringSet(lapply(mirna_dna,function(s) subseq(s,1,7)))
+seed8   <- DNAStringSet(lapply(mirna_dna,function(s) subseq(s,1,8)))
+dict6   <- PDict(reverseComplement(seed6))
+dict7m8 <- PDict(reverseComplement(seed7m8))
+dict7A1 <- PDict(reverseComplement(seed7A1))
+dict8   <- PDict(reverseComplement(seed8))
+
+# Scan UTRs
+res_list <- list()
+for (i in seq_along(utr_set)) {
+  gene <- names(utr_set)[i]
+  subj <- utr_set[[i]]
+  hits <- data.table()
+  h6 <- matchPDict(dict6,subj)
+  if (any(lengths(h6)>0)) hits <- rbind(hits,data.table(Target_Gene=gene,
+                                                        miRNA=rep(names(seed6),lengths(h6)),
+                                                        bs_start=start(unlist(h6)),
+                                                        bs_end=end(unlist(h6)),
+                                                        seed_class="6mer"))
+  h7m8 <- matchPDict(dict7m8,subj)
+  if (any(lengths(h7m8)>0)) hits <- rbind(hits,data.table(Target_Gene=gene,
+                                                          miRNA=rep(names(seed7m8),lengths(h7m8)),
+                                                          bs_start=start(unlist(h7m8)),
+                                                          bs_end=end(unlist(h7m8)),
+                                                          seed_class="7mer-m8"))
+  h7A1 <- matchPDict(dict7A1,subj)
+  if (any(lengths(h7A1)>0)) hits <- rbind(hits,data.table(Target_Gene=gene,
+                                                          miRNA=rep(names(seed7A1),lengths(h7A1)),
+                                                          bs_start=start(unlist(h7A1)),
+                                                          bs_end=end(unlist(h7A1)),
+                                                          seed_class="7mer-A1"))
+  h8 <- matchPDict(dict8,subj)
+  if (any(lengths(h8)>0)) hits <- rbind(hits,data.table(Target_Gene=gene,
+                                                        miRNA=rep(names(seed8),lengths(h8)),
+                                                        bs_start=start(unlist(h8)),
+                                                        bs_end=end(unlist(h8)),
+                                                        seed_class="8mer"))
+  if (nrow(hits)>0) res_list[[i]] <- hits
 }
+all_hits <- rbindlist(res_list,use.names=TRUE,fill=TRUE)
 
-# Run match finding for all validated miRNA-gene pairs
-all_matches <- list()
-for (i in seq_len(nrow(interactions))) {
-  miRNA <- interactions$miRNA[i]
-  gene <- interactions$`Target Gene`[i]
-  if (!(miRNA %in% names(mirna_seeds_dna))) next
-  
-  result <- find_genomic_matches(miRNA, mirna_seeds_dna[[miRNA]], gene, utr_data)
-  if (!is.null(result)) {
-    all_matches[[paste(miRNA, gene, sep = "_")]] <- result
-  }
+# Keep only validated pairs
+all_hits[, gene_miRNA := paste(Target_Gene,miRNA,sep="_")]
+interactions[, gene_miRNA := paste(Target_Gene,miRNA,sep="_")]
+hits <- all_hits[gene_miRNA %in% interactions$gene_miRNA]
+hits[, gene_miRNA := NULL]
+
+# Merge transcript info
+hits <- merge(hits,
+              utr_data[,.(Target_Gene=hgnc_symbol,
+                          ensembl_transcript_id,
+                          transcript_length,
+                          utr_len,
+                          `3utr_start_tx`=transcript_start)],
+              by="Target_Gene",all.x=TRUE)
+
+# Map to genomic coordinates
+utr_by_tx <- threeUTRsByTranscript(EnsDb.Hsapiens.v86)
+strip_ver <- function(x) sub("\\..*$","",x)
+names(utr_by_tx) <- strip_ver(names(utr_by_tx))
+hits[, ensembl_transcript_id := strip_ver(ensembl_transcript_id)]
+hits <- hits[!is.na(ensembl_transcript_id) & nzchar(ensembl_transcript_id)]
+hits_split <- split(hits,by="ensembl_transcript_id",keep.by=TRUE)
+
+mapped_list <- list()
+for (i in seq_along(hits_split)) {
+  tx_hits <- hits_split[[i]]
+  tx_id <- unique(tx_hits$ensembl_transcript_id)[1]
+  if (!(tx_id %in% names(utr_by_tx))) next
+  utr_len_tx <- sum(width(utr_by_tx[[tx_id]]))
+  tx_hits <- tx_hits[bs_start>=1L & bs_end<=utr_len_tx]
+  if (!nrow(tx_hits)) next
+  gr_tx <- GRanges(seqnames=Rle(rep(tx_id,nrow(tx_hits))),
+                   ranges=IRanges(start=tx_hits$bs_start,end=tx_hits$bs_end))
+  mapped <- mapFromTranscripts(gr_tx,utr_by_tx)
+  if (length(mapped)==0L) next
+  gr_dt <- as.data.table(mapped)
+  xh <- mcols(mapped)$xHits
+  thits <- cbind(tx_hits[xh],
+                 chromosome=as.character(gr_dt$seqnames),
+                 genomic_start=gr_dt$start,
+                 genomic_end=gr_dt$end,
+                 target_strand=as.character(gr_dt$strand))
+  mapped_list[[i]] <- thits
 }
+hits_expanded <- unique(rbindlist(mapped_list,use.names=TRUE,fill=TRUE))
 
-# Combine all results and prepare BED format
-final_results <- do.call(rbind, all_matches)
-final_results$chr <- paste0("chr", final_results$Chromosome)
-final_results$strand <- ifelse(final_results$Strand == -1, "-", "+")
-final_results$info <- paste(final_results$miRNA, final_results$Target_Gene, final_results$chr,
-                            final_results$genomic_start, final_results$genomic_end,
-                            final_results$strand, sep = "_")
+# Export BED and CSV
+hits_expanded[, chromosome := ifelse(grepl("^chr",chromosome),chromosome,paste0("chr",chromosome))]
+hits_expanded[, target_start := genomic_start-1L]
+hits_expanded[, target_end := genomic_end]
+hits_expanded[, name := paste(miRNA,Target_Gene,chromosome,target_start,target_end,target_strand,sep="_")]
 
-bed_df <- final_results[, c("chr", "genomic_start", "genomic_end", "strand", "info")]
+out <- hits_expanded[,.(chromosome,target_start,target_end,
+                        target_strand,name,seed_class,
+                        ensembl_transcript_id)]
 
-# Save as BED file
-write.table(bed_df, "mirna_genomic_binding_sites.bed", col.names = FALSE,
-            row.names = FALSE, quote = FALSE, sep = "\t")
+fwrite(out,"mirna_pairs_genomic.bed",sep="\t",col.names=TRUE,quote=FALSE)
--- code/09_compensatory_variants.R
+++ code/09_compensatory_variants.R
@@ -1,88 +1,131 @@
+# Load required libraries
 library(data.table)
 library(dplyr)
 library(tidyr)
+library(ggplot2)
 
-# Load target site variants
-targets <- fread("data/all_target_variants.csv")
-# Load miRNA binding site BED data
-mirna_bs_bed <- fread("data/mirna_genomic_binding_sites.bed")
+# Load variants
+targets <- fread("combined_target_variants.tsv")
+colnames(targets) <- c("Chr","Start","ID","Reference","Alternative","Quality","Check","Allele_count","Allele_total","Allele_freq",
+                       "AC_genomes","AN_genomes","AF_genomes","AC_exomes","AN_exomes","AF_exomes","grpmax_joint","AF_joint_afr",
+                       "AF_joint_amr","AF_joint_asj","AF_joint_eas","AF_joint_fin","AF_joint_nfe","AF_joint_mid","AF_joint_sas",
+                       "AF_joint_remaining","AF_exomes_afr","AF_exomes_amr","AF_exomes_asj","AF_exomes_eas","AF_exomes_fin","AF_exomes_nfe",
+                       "AF_exomes_mid","AF_exomes_sas","AF_exomes_remaining","AF_genomes_afr","AF_genomes_amr","AF_genomes_asj","AF_genomes_eas",
+                       "AF_genomes_fin","AF_genomes_nfe","AF_genomes_mid","AF_genomes_sas","AF_genomes_remaining")
+targets <- targets[targets$Check == "PASS",]
 
-colnames(targets) <- c(
-  "Chr", "Start", "ID", "Reference", "Alternative", "Quality", "Check", "Allele_count", "Allele_total", "Allele_freq",
-  "AC_genomes", "AN_genomes", "AF_genomes", "AC_exomes", "AN_exomes", "AF_exomes", "grpmax_joint", "AF_joint_afr",
-  "AF_joint_amr", "AF_joint_asj", "AF_joint_eas", "AF_joint_fin", "AF_joint_nfe", "AF_joint_mid", "AF_joint_sas",
-  "AF_joint_remaining", "AF_exomes_afr", "AF_exomes_amr", "AF_exomes_asj", "AF_exomes_eas", "AF_exomes_fin", "AF_exomes_nfe",
-  "AF_exomes_mid", "AF_exomes_sas", "AF_exomes_remaining", "AF_genomes_afr", "AF_genomes_amr", "AF_genomes_asj", "AF_genomes_eas",
-  "AF_genomes_fin", "AF_genomes_nfe", "AF_genomes_mid", "AF_genomes_sas", "AF_genomes_remaining"
-)
-# Keep only high-quality variants
-targets <- targets[Check == "PASS"]
-# Rename columns for joining
-setnames(mirna_bs_bed, old = c("V1", "V2", "V3"), new = c("Chr", "StartRange", "EndRange"))
+# Load miRNA binding sites
+mirna_bs_bed <- fread("mirna_pairs_genomic.bed")
+
+# Harmonize chromosomes
+setDT(targets); setDT(mirna_bs_bed)
+mirna_bs_bed[, Chr := sub("^chr","",chromosome)]
+targets[, Chr := sub("^chr","",Chr)]
+
+# Cast types and add position
+mirna_bs_bed[, `:=`(target_start=as.integer(target_start),
+                    target_end=as.integer(target_end))]
 targets[, position := Start]
 
-# Join to associate variants with miRNA binding sites
-targets_with_mirna <- mirna_bs_bed[targets, on = .(Chr, StartRange <= Start, EndRange >= Start), nomatch = NA]
-targets_with_mirna <- targets_with_mirna[, !c("StartRange", "EndRange", "V4"), with = FALSE]
+# Interval join
+targets_with_mirna <- mirna_bs_bed[
+  targets, on=.(Chr, target_start <= Start, target_end >= Start), nomatch=0L
+]
 
-# Extract metadata from BED annotation
+# Parse name field
 targets_split <- separate(
-  targets_with_mirna, col = V5,
-  into = c("mirna_name", "gene", "chromosome", "target_start", "target_end", "target_strand"),
-  sep = "_", remove = TRUE
+  targets_with_mirna,
+  col=name,
+  into=c("mirna_name","gene","chromosome_name","target_start_name","target_end_name","target_strand_name"),
+  sep="_", remove=FALSE
 )
+setDT(targets_split)
+targets_split[, `:=`(
+  target_start_name = as.integer(target_start_name),
+  target_end_name   = as.integer(target_end_name)
+)]
+targets_split[, bs_position := ifelse(target_strand=="+",
+                                      position - target_start + 1L,
+                                      target_end - position + 1L)]
 
-# Convert to numeric
-targets_split$target_start <- as.integer(targets_split$target_start)
-targets_split$target_end <- as.integer(targets_split$target_end)
+# Load conservation data
+mirna_variants <- fread("genome_variants_ocs.csv")
+mirna_variants <- mirna_variants[Seed=="Yes",]
+mirna_variants$bs_position <- mirna_variants$position-1
 
-# Compute relative binding site position
-targets_split$bs_position <- ifelse(
-  targets_split$target_strand == "+",
-  targets_split$position - targets_split$target_start + 1,
-  targets_split$target_end - targets_split$position + 1
-)
+# Merge
+pairs <- merge(targets_split, mirna_variants,
+               by=c("mirna_name","bs_position"),
+               suffixes=c("_TAR","_MIR"))
 
-# Load miRNA variants affecting the seed region
-mirna_variants <- fread("data/mirna_variants_ocs.csv")
-mirna_variants <- mirna_variants[Seed == "Yes"]
-mirna_variants[, bs_position := position - 1]
-
-# Merge miRNA and target variants by miRNA name and relative position
-pairs <- merge(
-  targets_split, mirna_variants,
-  by = c("mirna_name", "bs_position"),
-  suffixes = c("_TAR", "_MIR")
-)
-
-# Filter for reference alleles of the same purine/pyrimidine class
+# Filter
 filtered_pairs <- pairs %>%
   filter(
-    (Reference_MIR %in% c("A", "T") & Reference_TAR %in% c("A", "T")) |
-      (Reference_MIR %in% c("C", "G") & Reference_TAR %in% c("C", "G"))
-  ) %>%
-  filter(
-    nchar(Reference_TAR) == 1,
-    nchar(Reference_MIR) == 1,
-    nchar(Alternative_TAR) == 1,
-    nchar(Alternative_MIR) == 1
+    (Reference_MIR %in% c("A","T") & Reference_TAR %in% c("A","T")) |
+      (Reference_MIR %in% c("C","G") & Reference_TAR %in% c("C","G"))
   )
+filtered_pairs <- filtered_pairs[nchar(Reference_TAR)==1 &
+                                   nchar(Reference_MIR)==1 &
+                                   nchar(Alternative_TAR)==1 &
+                                   nchar(Alternative_MIR)==1,]
 
-# Keep strand-aware matched reference allele pairs
+# Strand-aware filtering
 filtered_pairs_strand <- filtered_pairs %>%
   filter(
-    (Strand_mirna == target_strand & Reference_TAR == Reference_MIR) |
-      (Strand_mirna != target_strand & Reference_TAR != Reference_MIR)
+    (Strand_mirna==target_strand & Reference_TAR==Reference_MIR) |
+      (Strand_mirna!=target_strand & Reference_TAR!=Reference_MIR)
   )
-
-# Filter for compensatory or identical alternative alleles
 filtered_pairs_final <- filtered_pairs_strand %>%
   filter(
-    (Strand_mirna == target_strand & Alternative_MIR == Alternative_TAR) |
-      (Strand_mirna != target_strand & (
-        (Alternative_MIR == "A" & Alternative_TAR == "T") |
-          (Alternative_MIR == "T" & Alternative_TAR == "A") |
-          (Alternative_MIR == "C" & Alternative_TAR == "G") |
-          (Alternative_MIR == "G" & Alternative_TAR == "C")
+    (Strand_mirna==target_strand & Alternative_MIR==Alternative_TAR) |
+      (Strand_mirna!=target_strand & (
+        (Alternative_MIR=="A" & Alternative_TAR=="T") |
+          (Alternative_MIR=="T" & Alternative_TAR=="A") |
+          (Alternative_MIR=="C" & Alternative_TAR=="G") |
+          (Alternative_MIR=="G" & Alternative_TAR=="C")
       ))
   )
+
+# Synonymous/non-synonymous
+variant_data <- filtered_pairs_strand %>%
+  mutate(synonymous = ifelse(do.call(paste0,.) %in% do.call(paste0,filtered_pairs_final),"yes","no"))
+mirna_variant_counts <- variant_data %>%
+  group_by(mirna_name, OCS, synonymous) %>%
+  summarise(count=n(), .groups="drop") %>%
+  pivot_wider(names_from=synonymous, values_from=count, values_fill=list(count=0)) %>%
+  rename(synonymous=yes, non_synonymous=no) %>%
+  mutate(ratio_syn_nonSyn = ifelse(non_synonymous>0, synonymous/non_synonymous, NA))
+
+
+# Allele frequency fix
+filtered_pairs_final <- filtered_pairs_final %>%
+  mutate(across(matches("^(AF_|Allele)"), ~ as.numeric(replace(., .==".","0"))))
+
+# Build plotting data
+plotting_data <- filtered_pairs_final[,c("Chr_MIR","Start_mirna","End_mirna",
+                                         "Chr_TAR","target_start","target_end","target_strand",
+                                         "Allele_freq_MIR","Allele_freq_TAR",
+                                         "grpmax_joint_MIR","grpmax_joint_TAR",
+                                         "OCS","mirna_name","Strand_mirna","gene",
+                                         "bs_position","position_TAR","Start",
+                                         "Reference_TAR","Alternative_TAR","Reference_MIR","Alternative_MIR","seed_class")]
+plotting_data$Allele_freq_MIR <- as.numeric(plotting_data$Allele_freq_MIR)
+plotting_data$Allele_freq_TAR <- as.numeric(plotting_data$Allele_freq_TAR)
+plotting_data <- plotting_data %>%
+  mutate(snv_type = case_when(
+    Allele_freq_TAR > 0.01 & Allele_freq_MIR > 0.01 ~ "snp_both",
+    Allele_freq_TAR < 0.01 & Allele_freq_MIR < 0.01 ~ "rare_both",
+    TRUE ~ "mixed"
+  ))
+
+# Collapse seeds
+plotting_data[, seed_class := if ("7mer-A1" %in% seed_class & "7mer-m8" %in% seed_class) "8mer" else seed_class,
+              by=setdiff(names(plotting_data),"seed_class")]
+seed_rank <- c("6mer"=1,"7mer-A1"=2,"7mer-m8"=2,"8mer"=3)
+plotting_data[, rank := seed_rank[seed_class]]
+plotting_data_collapsed <- plotting_data[, .SD[which.max(rank)],
+                                         by=setdiff(names(plotting_data),c("seed_class","rank"))]
+
+# Export
+write.csv(plotting_data_collapsed,"compensatory_pairs_all.csv",row.names=FALSE)
+
